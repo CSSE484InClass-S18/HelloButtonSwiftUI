c7fc34e3b47d83fb586ee16cbdf901b9dda5ce7a
David Fisher <fisherds@rose-hulman.edu>
2022-03-10 09:19:52 -0500

    Initial Commit

---
+++ HelloButtonSwiftUI/HelloButtonSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloButtonSwiftUIApp.swift
+//  HelloButtonSwiftUI
+//
+//  Created by David Fisher on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloButtonSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
